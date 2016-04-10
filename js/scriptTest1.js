80a54bc729246f4d9261fc477be12bc8fb5c5c02
Lê Minh Quân <lunardrik@live.com>
2016-04-10 16:01:38 +0700

    add scriptTest1 file

---
+++ js/scriptTest1.js
@@ -0,0 +1,73 @@
+(function($) {
+    "use strict";
+    function loadJS(src) {
+        document.write('<script type="text/javascript" src="' + src + '"></script>');
+    }
+     
+    loadJS('https://cdn.jsdelivr.net/foundation/5.2.2/js/vendor/jquery.js');
+    loadJS('https://cdn.jsdelivr.net/foundation/5.2.2/js/foundation.min.js');
+    loadJS('https://cdn.jsdelivr.net/foundation/5.2.2/js/vendor/modernizr.js');
+
+
+    // ????????
+    //moment.locale('ja');
+    kintone.events.on("app.record.edit.show", function(e) {
+        $(".gaia-ui-actionmenu").append( "<input type ='button' id='getDetail' value = 'Get Detail' />  " )
+	     
+
+		$( "#getDetail" ).bind( "click", function() {
+			getDetailTable(2);
+		});
+  
+    });
+	
+	function getDetailTable(recordId) {
+		var apiUrl = "https://kintoneivsdemo.cybozu.com/k/v1/record.json?app=610&id={0}"
+		
+		$.getJSON({
+			type: "GET",
+			url: apiUrl.replace("{0}", recordId),
+			success: function (data) {
+				console.debug(data.record.Detail);
+			},
+			beforeSend: setHeader
+		});
+	}
+	
+	function setHeader(xhr) {
+        xhr.setRequestHeader('X-Cybozu-API-Token', 'UoMwTXQuscujiQQM5gUIo86j938rAEDj6Y4MrwxI');
+        xhr.setRequestHeader('X-Cybozu-Authorization', 'bnFuaHV0QGluZGl2aXN5cy5qcDpAN35+RWE9fg==');
+      }
+	
+// function sendDataApi(){
+                // var txtContent = "This message sent from kintone";
+				// var a = kintone.app.getFieldElements('dien_thoai');
+				  // var phoneNumbers = "";
+				  // for (var i = 0; i < a.length; i++) {
+				   // phoneNumbers += a[i].textContent + ",";
+				  // }
+				  // phoneNumbers = phoneNumbers.substr(0,phoneNumbers.length-1);
+// alert(phoneNumbers);
+			
+                // $.ajax(
+                // {
+                    // type: "GET",
+                    // url: "https://api7.esms.vn/MainService.svc/xml/SendMultipleSMS_v3?phone="+phoneNumbers
+                            // +"&Content="
+                    // +txtContent+"&apikey=902019D38A70013E343EE021461E66&secretkey=D49D7330332A6F8BEB6DD096234942"+ "&smstype=7",
+                    // dataType: "TEXT",
+                    // success: function (result) {
+						
+						// alert("Send successfully")
+                      
+                    // },
+                    // error: function (error) {
+						// alert("Errors")
+                        
+                    // }
+                // });
+            // }
+
+
+})(jQuery);
+
