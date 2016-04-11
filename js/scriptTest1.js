1ff14f44257b097d01e0bc42e924e76d210d925d
Lê Minh Quân <lunardrik@live.com>
2016-04-12 01:25:01 +0700

    update

--- js/scriptTest1.js
+++ js/scriptTest1.js
@@ -17,7 +17,44 @@
 		
 		// get app record id if available
 		var recordId = getParameterByName("record");
+		getDetailTableFromRecordId(record, recordId);
 		
+		return e;
+    });
+	
+	function lookupQuotation() {
+		var apiUrl = "/k/v1/records.json";
+		var record = kintone.app.record.get();
+		var lookupValue = record.record.Lookup_0.value;
+				
+		if (lookupValue == '' || typeof(lookupValue) == 'undefined') {
+			return;
+		}
+		
+		var result = $.ajax({
+					type: "GET",
+					dataType: "json",
+					url: apiUrl,
+					data: {
+						"app" : 610,
+						"query": "Quotation_No = \"" + lookupValue + "\""
+					},
+					beforeSend: setHeader,
+					async: false
+				}).responseJSON;
+				
+		if (result.records.length == 0) {
+			return;
+		}
+		
+		var recordId = result.records[0]["$id"].value;
+		
+		getDetailTableFromRecordId(record, recordId);
+		
+		kintone.app.record.set(record);
+	}
+	
+	function getDetailTableFromRecordId(record, recordId) {
 		// if app record id is available (the record is forwarded from Quotation app)
 		if (recordId != null && typeof(recordId) !== 'undefined') {
 			// lookup the data with the record id
@@ -26,19 +63,6 @@
 			
 			updateDetailTable(record, detail);
 		}
-		
-		return e;
-    });
-	
-	function lookupQuotation() {
-		var record = kintone.app.record.get();
-		var lookupValue = record.record.Lookup_0.value;
-				
-		if (lookupValue == '' || typeof(lookupValue) == 'undefined') {
-			return;
-		}
-		
-		
 	}
 	
 	function getDetailTableSync(recordId) {
